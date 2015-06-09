c1545385ecc6ee3648565681510023aac9290fbb
wanhuiconcordia <yao.wanhui@gmail.com>
2015-06-08 22:44:42 -0400

    deleted some not using files

--- src/retailer/WarehouseInterface.java
+++
@@ -1,6 +0,0 @@
-package retailer;
-import java.rmi.RemoteException;
-public interface WarehouseInterface extends java.rmi.Remote{
-	//public ItemShippingStatus shipGoods(ArrayList<Item> itemList, Customer customer);
-	public String notifyMe(String message) throws RemoteException;
-}
